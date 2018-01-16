087f9554fc651d1c9c1f3d9fe67f6bbf73d638cb
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2018-01-16 14:42:53 -0300

    finished airport, block based solution

--- airport/index.js
+++ airport/index.js
@@ -1,50 +1,91 @@
-var freeBlocks = [];
+function fillIndexesWith(fillObj, targetArray, indexesArr) {
 
-var usedBlocks = [];
+    indexesArr.forEach((index) => {
+        targetArray[index] = fillObj;
+    });
+};
 
-function initializeFreeBlocks(freeBlocksArray){
-  for(var i = 0; i < 72; i++)
-    freeBlockArray.push(i);
-}
+function scheduleDateToBlock(dateObj) {
 
-function scheduleDateToBlock(dateObj){
-    
-    var blockSum = dateObj.getHours() * 3 + Math.floor(dateObj.getMinutes() / 20) + 1;
-    
-    if(dateObj.getMinutes() % 20){
-      return [blockSum, blockSum + 1];
+    var blockSum = dateObj.getHours() * 3 + Math.floor(dateObj.getMinutes() / 20);
+
+    if (dateObj.getMinutes() % 20) {
+        return [blockSum, blockSum + 1];
     } else {
-      return [blockSum];  
-    }   
+        return [blockSum];
+    }
 }
 
 class Scheduler {
- // returns true if there’s room to schedule at ‘time’
-  couldScheduleAt(dateObj){
-  var candidates = scheduleDateToBlock(dateObj);
-  
-  candidates.reduce((accumulator, current) => {
-      accumulator && freeBlocks.find(currentElement => {
-          return currentElement === current;
-      })
-  })
- }
 
- // returns true if we successfully scheduled
- ScheduleAt(dateObj){
-    if(couldScheduleAt(dateObj)){
+    constructor() {
+        this.usedBlocks = [];
+    }
+
+
+    // returns true if there’s room to schedule at ‘time’
+    couldScheduleAt(dateObj) {
+        var candidates = scheduleDateToBlock(dateObj);
+        
+        
+        return !this.usedBlocks.find((currentElement) => {
+                return currentElement === candidates[0];
+            }) && !this.usedBlocks.find((currentElement) => {
+                return currentElement === candidates[candidates.length % 1];
+            })
         
     }
- }
 
- // Choose an available time to schedule at, and return that time
- Schedule();
+    // returns true if we successfully scheduled
+    scheduleAt(dateObj) {
+        let indexesArr = scheduleDateToBlock(dateObj);
 
- // returns true if we successfully unscheduled something
- UnscheduleAt(Date time);
+        if (this.couldScheduleAt(dateObj)) {
+            fillIndexesWith(dateObj, this.usedBlocks, indexesArr);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Choose an available time to schedule at, and return that time
+    schedule(){
+
+        let date = new Date();
+        let index = 0;
+
+        do{
+            index = this.usedBlocks.findIndex((current) => { return !current; });
+            date.setHours(index / 3);
+            date.setMinutes(index % 3);
+        }while(this.usedBlocks[index + 1]);
+
+        return date;
+    }
+
+    // returns true if we successfully unscheduled something
+    unscheduleAt(dateObj){
+
+        let out = false;
+
+        this.usedBlocks.forEach((current) => {
+            if(dateObj === current){
+                current = null;
+                out = true;
+            }
+        });
+
+        return out;
+    }
 }
 
-var d = new Date();
+var date = new Date();
+var scheduler = new Scheduler();
 
 
-console.log(scheduleDateToBlock(d));
+console.log(scheduleDateToBlock(date));
+console.log(scheduler.couldScheduleAt(date));
+console.log(scheduler.scheduleAt(date));
+console.log(scheduler.scheduleAt(scheduler.schedule()));
+
+console.log(scheduler.usedBlocks);
